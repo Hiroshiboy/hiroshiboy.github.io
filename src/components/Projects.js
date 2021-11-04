222754898feafc465627d8873d4b66cb3098965d
Neil Sano <neil.hiroshi.sano@gmail.com>
2021-11-04 11:36:59 -0400

    Images in the data.js had to be uploaded and retrieved with a link.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,19 +11,19 @@ export default function Projects() {
             Projects
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-white">
-            These are some of the projects I am currently working on.
+            These are the personal projects I am currently working on.
           </p>
         </div>
-        <div className="flex flex-wrap -m-4">
+        <div className="flex flex-wrap m-4">
           {projects.map((project) => (
             <a
               href={project.link}
-              key={project.title}
-              className="sm:w-1/2 w-100 p-4">
+              key={project.image}
+              className="sm:w-1/2 w-100  p-4">
               <div className="flex relative">
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  className="absolute inset-0 w-full h-full object-cover object-bottom"
                   src={project.image}
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
--- src/data.js
+++ src/data.js
@@ -3,8 +3,8 @@ export const projects = [
       title: "Monster Hunter Rise Web Scraper",
       subtitle: "Python and Scrapy",
       description:
-        "This project was developed as a support to my other project I am currently working on using both Python and the Scrapy framework to create an HTML Scraper to harvest data and output a JSON File",
-      image: "",
+        "This project was developed as a support to my other project I am currently working on using both Python and the Scrapy framework to output a JSON File",
+      image: "https://user-images.githubusercontent.com/49799250/140358923-46de9e74-52e8-4319-b6da-896052ed5987.jpg",
       link: "https://github.com/Hiroshiboy/MHRWikiScraper",
     },
     {
@@ -12,7 +12,7 @@ export const projects = [
       subtitle: "Node.js, AWS DynamoDB, Lambda and API Gateway",
       description:
         "Work in Progress! This project takes the data provided to it from the JSON file created from the Web Scraper and stores it in a NoSQL DynamoDB",
-      image: "WIP",
+      image: "https://user-images.githubusercontent.com/49799250/140349717-97c762e2-9778-4716-bd0b-9817eeb87e40.png",
       link: "https://github.com/Hiroshiboy/MonsterHunterRiseDB",
     },
 
